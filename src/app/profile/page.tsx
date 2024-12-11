5b571f77fd7cac7c6ecb0db5fa05a8f0c243041b
Filippo Spinella <fili.spin2003@gmail.com>
2024-12-11 21:07:20 +0100

    Rimuovi codice non utilizzato e semplifica la gestione della sessione nella pagina del profilo

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,19 +3,10 @@
 
 import { useSession } from 'next-auth/react';
 import { Button } from '@radix-ui/themes';
-import { useEffect, useState } from 'react';
 
 export default function HomePage() {
   const { data: session } = useSession();
-  const [board, setBoard] = useState<any>([]);
-
-  const handleSave = () => {
-    alert('Credenziali aggiornate con successo!');
-  };
-  console.log(session?.user.email);
-
   if (session) {
-    session.user.image = undefined;
     return (
       <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-6">
         <div className="w-full max-w-md rounded-lg bg-white p-6 shadow-md">
@@ -54,35 +45,12 @@ export default function HomePage() {
                 className="w-full rounded-lg border px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
-            <Button
-              className="w-full transform rounded-lg bg-blue-500 px-6 py-3 font-bold text-white shadow-md transition-all duration-200 hover:bg-blue-600 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-300"
-              onClick={handleSave}
-            >
+            <Button className="w-full transform rounded-lg bg-blue-500 px-6 py-3 font-bold text-white shadow-md transition-all duration-200 hover:bg-blue-600 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-300">
               Salva Modifiche
             </Button>
           </form>
         </div>
       </div>
     );
-  } else {
-    return (
-      <div className="flex min-h-screen items-center justify-center bg-gray-100">
-        <div className="rounded-lg bg-white p-8 text-center shadow-lg">
-          <h1 className="mb-4 text-4xl font-bold text-red-600">
-            503 Service Unavailable
-          </h1>
-          <p className="mb-4 text-gray-700">
-            La sessione non è rilevata. Riprova più tardi.
-          </p>
-          <button
-            onClick={() => window.location.reload()}
-            className="mt-4 rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
-          >
-            Ricarica Pagina
-          </button>
-        </div>
-      </div>
-    );
   }
-  // ...existing code...
 }
