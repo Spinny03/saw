d9d7643c510b9a202253b8c9d64d6a130c803240
Guracc <garaccioni.andrea@gmail.com>
2025-01-20 18:54:42 +0100

    Prova Undo

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -11,3 +11,27 @@ export async function GET() {
     headers: { 'Content-Type': 'application/json' },
   });
 }
+
+/*create a new user i dati sono nel body della post*/
+export async function POST(request: Request) {
+  const body = await request.json();
+  /*genera un imagine con avatar lettera e salvala nel blob storage*/
+  const randomColorForAvatar = Math.floor(Math.random() * 16777215).toString(
+    16
+  );
+  const imageUrl = `https://ui-avatars.com/api/?name=${body.name}+${body.surname}&background=random&color=${randomColorForAvatar}`;
+
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      name: body.name,
+      surname: body.surname,
+      password: body.password,
+    },
+  });
+
+  return new Response(JSON.stringify(user), {
+    status: 201,
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
