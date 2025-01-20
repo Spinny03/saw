7e295d236935b63c2d5e3056a8749e1004585e00
Guracc <garaccioni.andrea@gmail.com>
2025-01-20 22:02:20 +0100

    Implement user update functionality with error handling and dynamic avatar generation

--- src/app/api/user/[userId]/route.ts
+++ src/app/api/user/[userId]/route.ts
@@ -23,3 +23,46 @@ export async function GET(
     headers: { 'Content-Type': 'application/json' },
   });
 }
+
+export async function PUT(
+  request: Request,
+  { params }: { params: Promise<{ userId: string }> }
+) {
+  const { userId } = await params;
+
+  // Controlla che l'ID utente sia fornito
+  if (!userId) {
+    return new Response('User ID is required', { status: 400 });
+  }
+
+  const body = await request.json();
+  const { email, name, surname } = body;
+
+  const randomColorForAvatar = Math.floor(Math.random() * 16777215).toString(
+    16
+  );
+  const imageUrl =
+    name && surname
+      ? `https://ui-avatars.com/api/?name=${name}+${surname}&background=random&color=${randomColorForAvatar}`
+      : undefined;
+
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data: {
+        email,
+        name,
+        surname,
+        ...(imageUrl && { image: imageUrl }),
+      },
+    });
+
+    return new Response(JSON.stringify(updatedUser), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    console.error('Error updating user:', error);
+    return new Response('Error updating user', { status: 500 });
+  }
+}
